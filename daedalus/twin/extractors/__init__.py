b76bae5d4810060f2aa32e96e934d1eec874c6c2
KTY <kaya.yesilyurt@tu-dortmund.de>
2026-08-01 20:59:22 +0200

    feat(twin): expose ROOT metadata extractor

--- daedalus/twin/extractors/__init__.py
+++ daedalus/twin/extractors/__init__.py
@@ -13,6 +13,15 @@ from .registry import (
     detect_language,
     registered_language_ids,
 )
+from .root_file_adapter import (
+    DEFAULT_ROOT_READ_LIMITS,
+    RootDataReport,
+    RootFieldRecord,
+    RootObjectRecord,
+    RootReadLimits,
+    RootReaderUnavailable,
+    inspect_root_artifact,
+)
 from .tree_sitter_adapter import (
     DEFAULT_PARSE_LIMITS,
     ParseLimits,
@@ -24,6 +33,7 @@ from .tree_sitter_adapter import (
 
 __all__ = [
     "DEFAULT_PARSE_LIMITS",
+    "DEFAULT_ROOT_READ_LIMITS",
     "LANGUAGE_SPECS",
     "ExtractorCapabilities",
     "ExtractorDiagnostic",
@@ -31,11 +41,17 @@ __all__ = [
     "LanguageDetection",
     "LanguageSpec",
     "ParseLimits",
+    "RootDataReport",
+    "RootFieldRecord",
+    "RootObjectRecord",
+    "RootReadLimits",
+    "RootReaderUnavailable",
     "SourceArtifact",
     "StructuralParseReport",
     "StructuralSymbol",
     "TreeSitterUnavailable",
     "detect_language",
+    "inspect_root_artifact",
     "parse_artifact",
     "registered_language_ids",
 ]
