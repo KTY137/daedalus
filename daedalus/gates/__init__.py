202add602ec58dee2293f445e29e8e16d128d0bc
KTY <kaya.yesilyurt@tu-dortmund.de>
2026-08-03 12:01:32 +0200

    feat(g0): export exact-head release assembly

--- daedalus/gates/__init__.py
+++ daedalus/gates/__init__.py
@@ -19,17 +19,20 @@ from .evidence_verifier import (
     evidence_requirements_sha256,
     strict_mechanical_blockers,
 )
+from .release import Gate0ReleaseReport, assemble_gate0_release_report
 from .report import GateReport, build_gate0_report, load_gate_report
 
 __all__ = [
     "ArtifactEvidence",
     "FaultMatrixEvidence",
+    "Gate0ReleaseReport",
     "GateEvidenceIndex",
     "GateReport",
     "OwnerDecisionEvidence",
     "ReviewEvidence",
     "RuntimeEnvelopeEvidence",
     "WorkflowRunEvidence",
+    "assemble_gate0_release_report",
     "assert_strict_exact_head",
     "build_gate0_report",
     "evidence_requirements_sha256",
