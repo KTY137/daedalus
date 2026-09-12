b629782eb6dbea36f2c007dce8761a9f60abfd72
KTY <kaya.yesilyurt@tu-dortmund.de>
2026-09-12 07:12:13 +0200

    feat(evolution): selectively port motif provenance contracts

---
+++ daedalus/evolution/__init__.py
@@ -0,0 +1,9 @@
+"""Non-promoting evolution contracts and research surfaces."""
+
+from .motifs import CrossRepositoryAlignment, MotifProvenance, MotifSupport
+
+__all__ = [
+    "CrossRepositoryAlignment",
+    "MotifProvenance",
+    "MotifSupport",
+]
